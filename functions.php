665c13a997e33225cdb5ef144b90e4bc7681800a
rclations <contact@rclations.com>
2017-07-12 14:22:09 -0400

    simplifying

--- functions.php
+++ functions.php
@@ -108,6 +108,33 @@ if ( ! function_exists( 'optionsframework_init' ) ) {
 }
 
 /**
+ * Add largo dashboard widget with help options
+ *
+ * @since 0.1
+ */
+function largo_dashboard_widgets() {
+   global $wp_meta_boxes;
+   wp_add_dashboard_widget( 'dashboard_quick_links', __( 'Project Largo Help', 'largo' ), 'largo_dashboard_quick_links' );
+}
+add_action('wp_dashboard_setup', 'largo_dashboard_widgets');
+
+function largo_dashboard_quick_links() {
+   echo '
+	   <div class="list-widget">
+		   <p>If you\'re having trouble with your site, want to request a new feature or are just interested in learning more about Project Largo, here are a few helpful links:</p>
+		   <ul>
+			   <li><a href="http://largoproject.org/">Largo Project Website</a></li>
+			   <li><a href="http://largo.readthedocs.io/">Largo Documentation</a></li>
+			   <li><a href="http://support.largoproject.org">Help Desk</a></li>
+			   <li><a href="http://support.largoproject.org/support/solutions">Knowledge Base</a></li>
+			   <li><a href="mailto:support@largoproject.org">Contact Us</a></li>
+		   </ul>
+		   <p>Developers can also log issues on <a href="https://github.com/INN/Largo">our public github repository</a> and if you would like to be included in our Largo users\' group, <a href="http://inn.us1.list-manage1.com/subscribe?u=81670c9d1b5fbeba1c29f2865&id=913028b23c">sign up here</a>.</p>
+	   </div>
+   ';
+}
+
+/**
  * A class to represent the one true Largo theme instance
  */
 class Largo {
@@ -150,7 +177,6 @@ class Largo {
 			'/inc/ajax-functions.php',
 			'/inc/helpers.php',
 			'/inc/plugin-init.php',
-			'/inc/dashboard.php',
 			'/inc/custom-feeds.php',
 			'/inc/users.php',
 			'/inc/term-meta.php',
