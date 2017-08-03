ef43df9147147c4ec9f3859ffff516c26c234828
rclations <contact@rclations.com>
2017-08-03 13:02:37 -0400

    Punctuation on admin notice

--- inc/update.php
+++ inc/update.php
@@ -65,7 +65,7 @@ function largo_update_admin_notice() {
 	if ( largo_need_updates() && 'dashboard_page_update-largo' !== $screen->id ) {
 		echo '<div class="notice notice-warning"><p>';
 			printf(
-				__( 'Largo has been updated! Please <a href="%s">visit the update page</a> to apply a required database update.', 'largo' ),
+				__( 'Largo has been updated. Please <a href="%s">visit the update page</a> to apply a required database update.', 'largo' ),
 				admin_url( 'index.php?page=update-largo' )
 			);
 		echo '</p></div>';
