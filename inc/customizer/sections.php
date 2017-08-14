baff8f66fe6f9e08f06e46545966142b81ea61ba
rclations <contact@rclations.com>
2017-08-14 18:20:04 -0400

    converting panels to sections & adding sample setting

--- inc/customizer/sections.php
+++ inc/customizer/sections.php
@@ -4,3 +4,40 @@
  *
  * @package Largo
  */
+
+/**
+ * Add a custom sections.
+ */
+function largo_customize_sections( $wp_customize ) {
+
+	$wp_customize->add_section( 'basic-settings', array(
+		'title'          => esc_html__( 'Basic Settings', 'largo' ),
+		'priority'       => 10,
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_section( 'theme-images', array(
+		'title'          => esc_html__( 'Theme Images', 'largo' ),
+		'priority'       => 10,
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_section( 'layout', array(
+		'title'          => esc_html__( 'Layout', 'largo' ),
+		'priority'       => 10,
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_section( 'navigation', array(
+		'title'          => esc_html__( 'Navigation', 'largo' ),
+		'priority'       => 10,
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_section( 'advanced', array(
+		'title'          => esc_html__( 'Advanced', 'largo' ),
+		'priority'       => 10,
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+}
+add_action( 'customize_register', 'largo_customize_sections' );
--- inc/customizer/settings.php
+++ inc/customizer/settings.php
@@ -4,3 +4,28 @@
  *
  * @package Largo
  */
+
+ /**
+  * Register additional scripts.
+  */
+ function _customize_additional_scripts( $wp_customize ) {
+ 	// Register a setting.
+ 	$wp_customize->add_setting(
+ 		'largo_header_scripts',
+ 		array(
+ 			'default'           => '',
+ 			'sanitize_callback' => 'force_balance_tags',
+ 		)
+ 	);
+ 	// Create the setting field.
+ 	$wp_customize->add_control(
+ 		'largo_header_scripts',
+ 		array(
+ 			'label'       => esc_html__( 'Header Scripts', 'largo' ),
+ 			'description' => esc_html__( 'Additional scripts to add to the header. Basic HTML tags are allowed.', 'largo' ),
+ 			'section'     => 'basic-settings',
+ 			'type'        => 'textarea',
+ 		)
+ 	);
+ }
+ // add_action( 'customize_register', '_customize_additional_scripts' );
