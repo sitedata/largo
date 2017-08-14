431c2599dd8bc4dca38bde1a5e67bb9cb2b0f5d1
rclations <contact@rclations.com>
2017-08-14 18:08:57 -0400

    adding panels to match old theme options

--- inc/customizer/panels.php
+++ inc/customizer/panels.php
@@ -7,5 +7,46 @@
 /**
  * Add a custom panels to attach sections too.
  */
-function largo_customize_panels( $wp_customize ) {}
+function largo_customize_panels( $wp_customize ) {
+
+	$wp_customize->add_panel( 'basic-settings', array(
+		'priority'       => 10,
+		'capability'     => 'edit_theme_options',
+		'theme_supports' => '',
+		'title'          => esc_html__( 'Basic Settings', 'largo' ),
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_panel( 'theme-images', array(
+		'priority'       => 10,
+		'capability'     => 'edit_theme_options',
+		'theme_supports' => '',
+		'title'          => esc_html__( 'Theme Images', 'largo' ),
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_panel( 'layout', array(
+		'priority'       => 10,
+		'capability'     => 'edit_theme_options',
+		'theme_supports' => '',
+		'title'          => esc_html__( 'Layout', 'largo' ),
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_panel( 'navigation', array(
+		'priority'       => 10,
+		'capability'     => 'edit_theme_options',
+		'theme_supports' => '',
+		'title'          => esc_html__( 'Navigation', 'largo' ),
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+
+	$wp_customize->add_panel( 'advanced', array(
+		'priority'       => 10,
+		'capability'     => 'edit_theme_options',
+		'theme_supports' => '',
+		'title'          => esc_html__( 'Advanced', 'largo' ),
+//		'description'    => esc_html__( '', 'largo' ),
+	) );
+}
 add_action( 'customize_register', 'largo_customize_panels' );
