36656f2e7c6a99e3e036ee3586142477b7007265
rclations <contact@rclations.com>
2017-08-15 16:33:05 -0400

    code cleanup

--- inc/customizer/settings.php
+++ inc/customizer/settings.php
@@ -5,27 +5,30 @@
  * @package Largo
  */
 
- /**
-  * Register additional scripts.
-  */
- function _customize_additional_scripts( $wp_customize ) {
- 	// Register a setting.
- 	$wp_customize->add_setting(
- 		'site_blurb',
- 		array(
- 			'default'           => '',
- 			'sanitize_callback' => 'force_balance_tags',
- 		)
- 	);
- 	// Create the setting field.
- 	$wp_customize->add_control(
- 		'site_blurb',
- 		array(
- 			'label'       => esc_html__( 'Site Description', 'largo' ),
- 			'description' => esc_html__( 'Enter a short blurb about your site. This is used in a sidebar widget.', 'largo' ),
- 			'section'     => 'basic_settings',
- 			'type'        => 'textarea',
- 		)
- 	);
- }
- add_action( 'customize_register', '_customize_additional_scripts' );
+/**
+ * Register additional scripts.
+ *
+ * @param obj $wp_customize Theme Customizer object.
+ */
+function largo_customize_additional_scripts( $wp_customize ) {
+
+	// Site Description.
+	$wp_customize->add_setting(
+		'site_blurb',
+		array(
+			'default'           => '',
+			'sanitize_callback' => 'force_balance_tags',
+		)
+	);
+	$wp_customize->add_control(
+		'site_blurb',
+		array(
+			'label'       => esc_html__( 'Site Description', 'largo' ),
+			'description' => esc_html__( 'Enter a short blurb about your site. This is used in a sidebar widget.', 'largo' ),
+			'section'     => 'basic_settings',
+			'type'        => 'textarea',
+		)
+	);
+
+}
+add_action( 'customize_register', 'largo_customize_additional_scripts' );
