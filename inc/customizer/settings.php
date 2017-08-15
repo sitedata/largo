3a9392da585a31b693a5d760ef5c413f61f1bccb
rclations <contact@rclations.com>
2017-08-15 16:30:13 -0400

    adding site_blurb field

--- inc/customizer/settings.php
+++ inc/customizer/settings.php
@@ -11,7 +11,7 @@
  function _customize_additional_scripts( $wp_customize ) {
  	// Register a setting.
  	$wp_customize->add_setting(
- 		'largo_header_scripts',
+ 		'site_blurb',
  		array(
  			'default'           => '',
  			'sanitize_callback' => 'force_balance_tags',
@@ -19,13 +19,13 @@
  	);
  	// Create the setting field.
  	$wp_customize->add_control(
- 		'largo_header_scripts',
+ 		'site_blurb',
  		array(
- 			'label'       => esc_html__( 'Header Scripts', 'largo' ),
- 			'description' => esc_html__( 'Additional scripts to add to the header. Basic HTML tags are allowed.', 'largo' ),
- 			'section'     => 'basic-settings',
+ 			'label'       => esc_html__( 'Site Description', 'largo' ),
+ 			'description' => esc_html__( 'Enter a short blurb about your site. This is used in a sidebar widget.', 'largo' ),
+ 			'section'     => 'basic_settings',
  			'type'        => 'textarea',
  		)
  	);
  }
- // add_action( 'customize_register', '_customize_additional_scripts' );
+ add_action( 'customize_register', '_customize_additional_scripts' );
