14bd39ec08398b6a083453b9e822e7bc82277b30
rclations <contact@rclations.com>
2017-08-16 15:37:59 -0400

    reorder customizer sections

--- inc/customizer/sections.php
+++ inc/customizer/sections.php
@@ -12,31 +12,31 @@ function largo_customize_sections( $wp_customize ) {
 
 	$wp_customize->add_section( 'basic_settings', array(
 		'title'          => esc_html__( 'Basic Settings', 'largo' ),
-		'priority'       => 10,
+		'priority'       => 21,
 //		'description'    => esc_html__( '', 'largo' ),
 	) );
 
 	$wp_customize->add_section( 'theme_images', array(
 		'title'          => esc_html__( 'Theme Images', 'largo' ),
-		'priority'       => 10,
+		'priority'       => 21,
 //		'description'    => esc_html__( '', 'largo' ),
 	) );
 
 	$wp_customize->add_section( 'layout', array(
 		'title'          => esc_html__( 'Layout', 'largo' ),
-		'priority'       => 10,
+		'priority'       => 21,
 //		'description'    => esc_html__( '', 'largo' ),
 	) );
 
 	$wp_customize->add_section( 'navigation', array(
 		'title'          => esc_html__( 'Navigation', 'largo' ),
-		'priority'       => 10,
+		'priority'       => 21,
 //		'description'    => esc_html__( '', 'largo' ),
 	) );
 
 	$wp_customize->add_section( 'advanced', array(
 		'title'          => esc_html__( 'Advanced', 'largo' ),
-		'priority'       => 10,
+		'priority'       => 21,
 //		'description'    => esc_html__( '', 'largo' ),
 	) );
 }
