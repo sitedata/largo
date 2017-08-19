7e7b1eff51c3ab14cf14fdf31c5b3695ccb4a441
rclations <contact@rclations.com>
2017-08-19 13:51:33 -0600

    converting instances of post_permalink() to get_permalink()

--- partials/archive-category-secondary-feature.php
+++ partials/archive-category-secondary-feature.php
@@ -1,15 +1,15 @@
 <article id="post-<?php echo $featured_post->ID; ?>" <?php post_class('span3', $featured_post->ID); ?>>
 	<?php if ( has_post_thumbnail($featured_post->ID) ) { ?>
 		<div class="<?php largo_hero_class($featured_post->ID); ?>">
-			<a href="<?php echo post_permalink($featured_post->ID); ?>">
+			<a href="<?php echo get_permalink( $featured_post->ID ); ?>">
 				<?php echo get_the_post_thumbnail($featured_post->ID, 'rect_thumb'); ?>
 			</a>
 		</div>
 	<?php } ?>
 
 	<h2 class="entry-title">
-		<a href="<?php echo post_permalink($featured_post->ID); ?>"
-			title="<?php echo __( 'Permalink to', 'largo' ) . esc_attr(strip_tags($featured_post->post_title)); ?>"	
+		<a href="<?php echo get_permalink( $featured_post->ID ); ?>"
+			title="<?php echo __( 'Permalink to', 'largo' ) . esc_attr(strip_tags($featured_post->post_title)); ?>"
 			rel="bookmark"><?php echo $featured_post->post_title; ?></a>
 	</h2>
 </article>
