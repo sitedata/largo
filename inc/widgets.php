818da7ce69a524901137008bb97379835e62d408
Ben Keith <ben@benlk.com>
2018-08-01 00:52:21 -0400

    Remove the Largo Staff widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -35,7 +35,6 @@ function largo_widgets() {
 		'largo_author_widget' => '/inc/widgets/largo-author-bio.php',
 		'largo_tag_list_widget' => '/inc/widgets/largo-tag-list.php',
 		'largo_prev_next_post_links_widget' => '/inc/widgets/largo-prev-next-post-links.php',
-		'largo_staff_widget' => '/inc/widgets/largo-staff.php'
 	);
 
 	// If series are enabled
