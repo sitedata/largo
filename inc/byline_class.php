deefc30148ab690e454b08a6d034e0eef9f9686b
Ben Keith <ben@benlk.com>
2018-11-07 22:57:36 -0500

    An undefined variable in the byline class, for https://github.com/INN/largo/issues/1547

--- inc/byline_class.php
+++ inc/byline_class.php
@@ -96,13 +96,13 @@ class Largo_Byline {
 	 * This supports both Largo_Byline and Largo_CoAuthors_Byline
 	 */
 	function avatar() {
-		
 		// only do avatars if it's a single post
 		if ( ! is_single() ) {
-			$output = '';
+			$output = ' ';
 		} else {
+			$output = '';
 			$author_email = get_the_author_meta( 'email', $this->author_id );
-			if ( $this->author->type == 'guest-author' && get_the_post_thumbnail( $this->author->ID ) ) {
+			if ( isset( $this->author ) && $this->author->type == 'guest-author' && get_the_post_thumbnail( $this->author->ID ) ) {
 				$output = get_the_post_thumbnail( $this->author->ID, array( 60,60 ) );
 				$output = str_replace( 'attachment-32x32', 'avatar avatar-32 photo', $output );
 				$output = str_replace( 'wp-post-image', '', $output );
