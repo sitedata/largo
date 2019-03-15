sitedata/largo

commit 351eda77968ad3a8c49bf7a25b499d211eb1822f

Remove extract() in largo_module_shortcode()

inc/editor.php
@@ -30,13 +30,13 @@ add_action( 'init', 'largo_add_mce_buttons' );
  * @since 0.3
  */
 function largo_module_shortcode( $atts, $content, $code ) {
-    extract( shortcode_atts( array(
+    $atts = shortcode_atts( array(
         'align' => 'left',
         'width' => 'half',
         'type' => 'aside',
-    ), $atts ) );
+    ), $atts );
 
-    return sprintf( '<aside class="module %s %s %s">%s</aside>', $type, $align, $width, $content );
+    return sprintf( '<aside class="module %s %s %s">%s</aside>', $atts['type'], $atts['align'], $atts['width'], $atts['content'] );
 }
 add_shortcode( 'module', 'largo_module_shortcode' );
 

commit fc05606e57b75f545e8cbf8d22ad4ce241457e4e

Fix bug in largo_module_shortcode where shortcode content was not being displayed
This fixes a bug introduced in https://github.com/INN/largo/commit/351eda77968ad3a8c49bf7a25b499d211eb1822f where $content was improperly changed to $atts['content'].

Fixes https://github.com/INN/largo/issues/1639

inc/editor.php
@@ -25,18 +25,27 @@ add_action( 'init', 'largo_add_mce_buttons' );
 
 /**
  * Add the module shortcode (used for pullquotes and asides within posts)
+ *
  * This is no longer used but is included here for backwards compatibility
+ *
+ * The module insertion is handled by largo/js/tinymce/plugins/largo/tinymce.js, which now inserts <aside> tags.
  *
  * @since 0.3
  */
 function largo_module_shortcode( $atts, $content, $code ) {
-    $atts = shortcode_atts( array(
-        'align' => 'left',
-        'width' => 'half',
-        'type' => 'aside',
-    ), $atts );
+	$atts = shortcode_atts( array(
+		'align' => 'left',
+		'width' => 'half',
+		'type' => 'aside',
+	), $atts );
 
-    return sprintf( '<aside class="module %s %s %s">%s</aside>', $atts['type'], $atts['align'], $atts['width'], $atts['content'] );
+	return sprintf(
+		'<aside class="module %s %s %s">%s</aside>',
+		$atts['type'],
+		$atts['align'],
+		$atts['width'],
+		$content
+	);
 }
 add_shortcode( 'module', 'largo_module_shortcode' );
 