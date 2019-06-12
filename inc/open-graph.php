a18f895e4cffb2b41e8705ea4f8e2d041f101708
Ben Keith <ben@benlk.com>
2019-06-12 17:46:46 -0400

    Apply WordPress PHPCS Code Standards to largo_opengraph()

--- inc/open-graph.php
+++ inc/open-graph.php
@@ -17,41 +17,47 @@ if ( ! function_exists( 'largo_opengraph' ) ) {
 
 		global $post;
 
-		// start the output, some attributes will be the same for all page types
+		// start the output; some attributes will be the same for all page types.
 		echo '<meta name="twitter:card" content="summary">';
 
-		if ( of_get_option( 'twitter_link' ) ) {
-			echo '<meta name="twitter:site" content="@' . largo_twitter_url_to_username( of_get_option( 'twitter_link' ) ) . '">';
+		$twitter_url =  largo_twitter_url_to_username( of_get_option( 'twitter_link' ) );
+		if ( ! empty( $twitter_url ) ) {
+			echo '<meta name="twitter:site" content="@' . esc_attr( $twitter_url ) . '">';
 		}
 
-		// output appropriate OG tags by page type
+		// output appropriate OG tags by page type.
 		if ( is_singular() ) {
 			if ( have_posts() ) {
-				the_post(); // we need to queue up the post to get the post specific info
-				
-				if ( get_the_author_meta( 'twitter' ) && !get_post_meta( $post->ID, 'largo_byline_text' ) )
-					echo '<meta name="twitter:creator" content="@' . largo_twitter_url_to_username( get_the_author_meta( 'twitter' ) ) . '">';
+				the_post(); // we need to queue up the post to get the post specific info.
+
+				if ( get_the_author_meta( 'twitter' ) && ! get_post_meta( $post->ID, 'largo_byline_text' ) ) {
+					echo '<meta name="twitter:creator" content="@' . esc_attr( largo_twitter_url_to_username( get_the_author_meta( 'twitter' ) ) ) . '">';
+				}
 				?>
 				<meta property="og:title" content="<?php the_title(); ?>" />
 				<meta property="og:type" content="article" />
 				<meta property="og:url" content="<?php the_permalink(); ?>"/>
-				<meta property="og:description" content="<?php echo strip_tags( esc_html( get_the_excerpt() ) ); ?>" />
-				<meta name="description" content="<?php echo strip_tags( esc_html( get_the_excerpt() ) ); ?>" />
+				<meta property="og:description" content="<?php echo esc_attr( wp_strip_all_tags( esc_html( get_the_excerpt() ) ) ); ?>" />
+				<meta name="description" content="<?php echo esc_attr( wp_strip_all_tags( esc_html( get_the_excerpt() ) ) ); ?>" />
 				<?php
 			} // have_posts
 
 			rewind_posts();
 
-		} else if ( is_front_page() ) {
+		} elseif ( is_front_page() ) {
+			printf(
+				'<meta property="og:title" content="%1$s - %2$s" />',
+				esc_attr( bloginfo( 'name' ) ),
+				esc_attr( bloginfo( 'description' ) )
+			);
 			?>
-				<meta property="og:title" content="<?php bloginfo( 'name' ); echo ' - '; bloginfo( 'description' ); ?>" />
 				<meta property="og:type" content="website" />
-				<meta property="og:url" content="<?php echo home_url(); ?>"/>
-				<meta property="og:description" content="<?php bloginfo( 'description' ); ?>" />
+				<meta property="og:url" content="<?php echo esc_attr( home_url() ); ?>"/>
+				<meta property="og:description" content="<?php esc_attr( bloginfo( 'description' ) ); ?>" />
 				<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
 			<?php
 		} else {
-			// not a single post, not the front page
+			// not a single post, not the front page.
 			?>
 				<meta property="og:title" content="<?php wp_title(); ?>" />
 				<meta property="og:type" content="article" />
@@ -59,47 +65,51 @@ if ( ! function_exists( 'largo_opengraph' ) ) {
 			<?php
 
 			$description = '';
-			//let's try to get a better description when available
+			// let's try to get a better description when available.
 			if ( is_category() && category_description() ) {
 				$description = category_description();
 			} elseif ( is_author() ) {
 				if ( have_posts() ) {
-					the_post(); // we need to queue up the post to get the post specific info
-					if ( get_the_author_meta( 'description' ) )
+					the_post(); // we need to queue up the post to get the post specific info.
+					if ( get_the_author_meta( 'description' ) ) {
 						$description = get_the_author_meta( 'description' );
+					}
 				}
 				rewind_posts();
 			} else {
 				$description = get_bloginfo( 'description' );
 			}
-			if ( !empty( $description ) ) {
-				echo '<meta property="og:description" content="' . strip_tags( esc_html( $description ) ) . '" />';
-				echo '<meta name="description" content="' . strip_tags( esc_html( $description ) ) . '" />';
+			if ( ! empty( $description ) ) {
+				echo '<meta property="og:description" content="' . esc_attr( wp_strip_all_tags( esc_html( $description ) ) ) . '" />';
+				echo '<meta name="description" content="' . esc_attr( wp_strip_all_tags( esc_html( $description ) ) ) . '" />';
 			}
 		}
 
-		// a few more attributes that are common to all page types
-		echo '<meta property="og:site_name" content="'  . get_bloginfo() . '" />';
+		// add a few more attributes that are common to all page types.
+		echo '<meta property="og:site_name" content="' . esc_attr( get_bloginfo() ) . '" />';
+
+		/*
+		 * Images
+		 */
 
 		// set a default thumbnail; if a post has a featured image use that instead.
 		if ( is_singular() && has_post_thumbnail( $post->ID ) ) {
 			$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 			$thumbnail_url = $image[0];
-		} else if ( of_get_option( 'logo_thumbnail_sq' ) ) {
+		} elseif ( ! empty( of_get_option( 'logo_thumbnail_sq' ) ) ) {
 			$thumbnail_url = of_get_option( 'logo_thumbnail_sq' );
 		} else {
 			$thumbnail_url = false;
 		}
 
-
-		// thumbnail url
+		// output the social media image.
 		if ( $thumbnail_url ) {
 			echo '<meta property="og:image" content="' . esc_attr( $thumbnail_url ) . '" />';
 		}
 
 	}
 }
-// don't add this if Yoast is active
+// don't add this if Yoast is active!
 if ( ! class_exists( 'WPSEO_OpenGraph' ) ) {
 	add_action( 'wp_head', 'largo_opengraph' );
 }
@@ -118,7 +128,7 @@ function largo_wp_title_parts_filter( $parts ) {
 	// Add the blog description for the home/front page.
 	if ( is_home() || is_front_page() ) {
 		$site_description = get_bloginfo( 'description', 'display' );
-		if ( ! empty ( $site_description ) ) {
+		if ( ! empty( $site_description ) ) {
 			$parts[] = $site_description;
 		}
 	}
