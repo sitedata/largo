b4dbf80a96fdfdfd8b3997a1f8da1105231362a2
Ben Keith <ben@benlk.com>
2019-06-12 17:34:04 -0400

    Whitespace changes in largo_opengraph for better function layout, group image functionality

--- inc/open-graph.php
+++ inc/open-graph.php
@@ -5,98 +5,97 @@
  * @package Largo
  */
 
-/**
- * Adds appropriate open graph, twittercards, and google publisher tags
- * to the header based on the page type displayed
- *
- * @uses largo_twitter_url_to_username()
- * @since 0.3
- */
 if ( ! function_exists( 'largo_opengraph' ) ) {
+	/**
+	 * Adds appropriate open graph, twittercards, and google publisher tags
+	 * to the header based on the page type displayed
+	 *
+	 * @uses largo_twitter_url_to_username()
+	 * @since 0.3
+	 */
 	function largo_opengraph() {
 
 		global $post;
 
-		// set a default thumbnail, if a post has a featured image use that instead
-		if ( is_singular() && has_post_thumbnail( $post->ID ) ) {
-			$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
-			$thumbnailURL = $image[0];
-		} else if ( of_get_option( 'logo_thumbnail_sq' ) ) {
-			$thumbnailURL = of_get_option( 'logo_thumbnail_sq' );
-		} else {
-			$thumbnailURL = false;
+		// start the output, some attributes will be the same for all page types
+		echo '<meta name="twitter:card" content="summary">';
+
+		if ( of_get_option( 'twitter_link' ) ) {
+			echo '<meta name="twitter:site" content="@' . largo_twitter_url_to_username( of_get_option( 'twitter_link' ) ) . '">';
 		}
 
-		// start the output, some attributes will be the same for all page types ?>
+		// output appropriate OG tags by page type
+		if ( is_singular() ) {
+			if ( have_posts() ) {
+				the_post(); // we need to queue up the post to get the post specific info
+				
+				if ( get_the_author_meta( 'twitter' ) && !get_post_meta( $post->ID, 'largo_byline_text' ) )
+					echo '<meta name="twitter:creator" content="@' . largo_twitter_url_to_username( get_the_author_meta( 'twitter' ) ) . '">';
+				?>
+				<meta property="og:title" content="<?php the_title(); ?>" />
+				<meta property="og:type" content="article" />
+				<meta property="og:url" content="<?php the_permalink(); ?>"/>
+				<meta property="og:description" content="<?php echo strip_tags( esc_html( get_the_excerpt() ) ); ?>" />
+				<meta name="description" content="<?php echo strip_tags( esc_html( get_the_excerpt() ) ); ?>" />
+				<?php
+			} // have_posts
 
-		<meta name="twitter:card" content="summary">
+			rewind_posts();
 
-		<?php
-			if ( of_get_option( 'twitter_link' ) )
-				echo '<meta name="twitter:site" content="@' . largo_twitter_url_to_username( of_get_option( 'twitter_link' ) ) . '">';
-		?>
+		} else if ( is_front_page() ) {
+			?>
+				<meta property="og:title" content="<?php bloginfo( 'name' ); echo ' - '; bloginfo( 'description' ); ?>" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="<?php echo home_url(); ?>"/>
+				<meta property="og:description" content="<?php bloginfo( 'description' ); ?>" />
+				<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
+			<?php
+		} else {
+			// not a single post, not the front page
+			?>
+				<meta property="og:title" content="<?php wp_title(); ?>" />
+				<meta property="og:type" content="article" />
+				<meta property="og:url" content="<?php echo esc_url( largo_get_current_url() ); ?>"/>
+			<?php
 
-		<?php // output appropriate OG tags by page type
-			if ( is_singular() ) {
+			$description = '';
+			//let's try to get a better description when available
+			if ( is_category() && category_description() ) {
+				$description = category_description();
+			} elseif ( is_author() ) {
 				if ( have_posts() ) {
 					the_post(); // we need to queue up the post to get the post specific info
-					
-					if ( get_the_author_meta( 'twitter' ) && !get_post_meta( $post->ID, 'largo_byline_text' ) )
-						echo '<meta name="twitter:creator" content="@' . largo_twitter_url_to_username( get_the_author_meta( 'twitter' ) ) . '">';
-					?>
-					<meta property="og:title" content="<?php the_title(); ?>" />
-					<meta property="og:type" content="article" />
-					<meta property="og:url" content="<?php the_permalink(); ?>"/>
-					<meta property="og:description" content="<?php echo strip_tags( esc_html( get_the_excerpt() ) ); ?>" />
-					<meta name="description" content="<?php echo strip_tags( esc_html( get_the_excerpt() ) ); ?>" />
-					<?php
-				} // have_posts
-
+					if ( get_the_author_meta( 'description' ) )
+						$description = get_the_author_meta( 'description' );
+				}
 				rewind_posts();
-
-			} elseif ( is_front_page() ) {
-				?>
-					<meta property="og:title" content="<?php bloginfo( 'name' ); echo ' - '; bloginfo( 'description' ); ?>" />
-					<meta property="og:type" content="website" />
-					<meta property="og:url" content="<?php echo home_url(); ?>"/>
-					<meta property="og:description" content="<?php bloginfo( 'description' ); ?>" />
-					<meta name="description" content="<?php bloginfo( 'description' ); ?>" />
-				<?php
 			} else {
-				// not a single post, not the front page
-				?>
-					<meta property="og:title" content="<?php wp_title(); ?>" />
-					<meta property="og:type" content="article" />
-					<meta property="og:url" content="<?php echo esc_url( largo_get_current_url() ); ?>"/>
-				<?php
-
-				$description = '';
-				//let's try to get a better description when available
-				if ( is_category() && category_description() ) {
-					$description = category_description();
-				} elseif ( is_author() ) {
-					if ( have_posts() ) {
-						the_post(); // we need to queue up the post to get the post specific info
-						if ( get_the_author_meta( 'description' ) )
-							$description = get_the_author_meta( 'description' );
-					}
-					rewind_posts();
-				} else {
-					$description = get_bloginfo( 'description' );
-				}
-				if ( !empty( $description ) ) {
-					echo '<meta property="og:description" content="' . strip_tags( esc_html( $description ) ) . '" />';
-					echo '<meta name="description" content="' . strip_tags( esc_html( $description ) ) . '" />';
-				}
+				$description = get_bloginfo( 'description' );
 			}
-
-			// a few more attributes that are common to all page types
-			echo '<meta property="og:site_name" content="'  . get_bloginfo() . '" />';
-
-			// thumbnail url
-			if ( $thumbnailURL ) {
-				echo '<meta property="og:image" content="' . esc_url( $thumbnailURL ) . '" />';
+			if ( !empty( $description ) ) {
+				echo '<meta property="og:description" content="' . strip_tags( esc_html( $description ) ) . '" />';
+				echo '<meta name="description" content="' . strip_tags( esc_html( $description ) ) . '" />';
 			}
+		}
+
+		// a few more attributes that are common to all page types
+		echo '<meta property="og:site_name" content="'  . get_bloginfo() . '" />';
+
+		// set a default thumbnail; if a post has a featured image use that instead.
+		if ( is_singular() && has_post_thumbnail( $post->ID ) ) {
+			$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
+			$thumbnail_url = $image[0];
+		} else if ( of_get_option( 'logo_thumbnail_sq' ) ) {
+			$thumbnail_url = of_get_option( 'logo_thumbnail_sq' );
+		} else {
+			$thumbnail_url = false;
+		}
+
+
+		// thumbnail url
+		if ( $thumbnail_url ) {
+			echo '<meta property="og:image" content="' . esc_attr( $thumbnail_url ) . '" />';
+		}
 
 	}
 }
