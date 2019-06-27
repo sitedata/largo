3aa81e9f8ede577343c2b5dbe146573fbbdd6b5f
Josh Darby <josh@inn.org>
2019-06-27 15:23:47 -0400

    Added more checks before adding in our media credit/url

--- js/blocks-image-customization.js
+++ js/blocks-image-customization.js
@@ -16,27 +16,39 @@ function largo_core_image_block_add_media_credit( element, blockType, attribute 
 
         var media_response = media.fetch().then( function( media ) {
 
+            // the original instance of the media caption
             var attachment_caption = attribute.caption;
 
-            if( attachment_caption.includes( media.media_credit ) ){
+            if( media.media_credit._media_credit ){
 
-                return;
+                // our default media credit, if it exists 
+                var media_credit = '<span class="largo-attachment-media-credit">' + media.media_credit._media_credit + '</span>';
 
-            } else {
-
-                var media_credit = media.media_credit;
-
+                // if our media credit has a url, include that also
                 if( media.media_credit._media_credit_url ){
 
-                    var media_credit = '<a href="'+media.media_credit._media_credit_url+'" target="_blank">'+media.media_credit._media_credit+'</a>';
+                    var media_credit = '<span class="largo-attachment-media-credit"><a href="'+media.media_credit._media_credit_url+'" target="_blank" rel="noopener noreferrer">'+media.media_credit._media_credit+'</a></span>';
 
                 }
 
-                var attachment_caption = attachment_caption + '<br/><span class="largo-attachment-media-credit">' + media_credit + '</span>';
+                // if our media attachment caption already includes `largo-attachment-media-credit`,
+                // which is the class of our span, don't do anything more
+                if( attachment_caption.includes( 'largo-attachment-media-credit' ) ){
 
-                return Object.assign( attribute, { caption: attachment_caption } );
+                    return;
+                
+                // else, our media credit probably doesn't exist. let's add it in
+                } else {
+
+                    var attachment_caption = attachment_caption + '<br/>' + media_credit;
+
+                    // return our attribute with its new caption
+                    return Object.assign( attribute, { caption: attachment_caption } );
+
+                }
 
             }
+            
         });
     }
 }
