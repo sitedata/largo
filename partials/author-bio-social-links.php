4aafc905dc662ad89618bafa22873c8ad9467e50
Nguyen, Duc <vanducld@gmail.com>
2019-10-08 23:09:26 +0700

    feat: #1791 Add and correct title

--- partials/author-bio-social-links.php
+++ partials/author-bio-social-links.php
@@ -23,25 +23,25 @@ if (isset($user_meta['show_email'][0])) {
 <ul class="social-links">
 	<?php if ( $fb = $author_obj->fb ) { ?>
 		<li class="facebook">
-			<a href="https://facebook.com/<?php echo largo_fb_url_to_username(esc_url( $fb )); ?>" title="<?php echo esc_attr( $author_obj->display_name ); ?> on Facebook" rel="me"><i class="icon-facebook"></i></a>
+			<a href="https://facebook.com/<?php echo largo_fb_url_to_username(esc_url( $fb )); ?>" title="Follow <?php echo esc_attr( $author_obj->display_name ); ?> on Facebook" rel="me"><i class="icon-facebook"></i></a>
 		</li>
 	<?php } ?>
 
 	<?php if ( $twitter = $author_obj->twitter ) { ?>
 		<li class="twitter">
-			<a href="https://twitter.com/<?php echo largo_twitter_url_to_username ( $twitter ); ?>"><i class="icon-twitter"></i></a>
+			<a href="https://twitter.com/<?php echo largo_twitter_url_to_username ( $twitter ); ?>" title="Follow <?php echo largo_twitter_url_to_username ( $twitter ); ?> on Twitter" ><i class="icon-twitter"></i></a>
 		</li>
 	<?php } ?>
 
 	<?php if ( $email && $show_email !== 'off' ) { ?>
 		<li class="email">
-			<a class="email" href="mailto:<?php echo esc_attr( $email ); ?>" title="e-mail <?php echo esc_attr( $author_obj->display_name ); ?>"><i class="icon-mail"></i></a>
+			<a class="email" href="mailto:<?php echo esc_attr( $email ); ?>" title="E-mail <?php echo esc_attr( $author_obj->display_name ); ?>"><i class="icon-mail"></i></a>
 		</li>
 	<?php } ?>
 
 	<?php if ( $linkedin = $author_obj->linkedin ) { ?>
 		<li class="linkedin">
-			<a href="<?php echo esc_url( $linkedin ); ?>" title="<?php echo esc_attr( $author_obj->display_name ); ?> on LinkedIn"><i class="icon-linkedin"></i></a>
+			<a href="<?php echo esc_url( $linkedin ); ?>" title="Follow <?php echo esc_attr( $author_obj->display_name ); ?> on LinkedIn"><i class="icon-linkedin"></i></a>
 		</li>
 	<?php } ?>
 
