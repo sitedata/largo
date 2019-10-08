bb80fc9127dae616128c7bd68b37eb5c1a1b3be5
Nguyen, Duc <vanducld@gmail.com>
2019-10-08 22:04:25 +0700

    feat: #1785 Remove maximum scale

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
 	/**
 	 * The template for displaying the header
